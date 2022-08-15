3494210e0e42e3ee22f0d9d0b9710bcc4fc7990c
Mohamed A. Gamal <m.metallized@gmail.com>
2022-08-15 19:29:20 +0200

    EDIT: Route table & Route
    Configured Route Table and Default route to access internet

--- vpc.tf
+++ vpc.tf
@@ -29,3 +29,19 @@ resource "aws_internet_gateway" "mtc_internet_gateway" {
         name = "dev-igw"
     }
 }
+
+#Creating a route table
+resource "aws_route_table" "mtc_public_rt" {
+    vpc_id = aws_vpc.mtc_vpc.id
+
+    tags = {
+        name = "dev-public_rt"
+    }
+}
+
+# Creating a default route for accessing internet
+resource "aws_route" "default_route" {
+    route_table_id = aws_route_table.mtc_public_rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+}
